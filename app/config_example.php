e384f91d37b745ed818ce557ddef4aa398c07d41
minhsieh-root <side112358@gmail.com>
2018-06-15 10:16:44 +0800

    add config_example

--- app/config_example.php
+++ app/config_example.php
@@ -4,7 +4,7 @@ define("FB_APP_ID", "42146222222222227");
 define("FB_APP_SEC", "1a12bdsd87f6as8d6fa544b876d");
 define("FB_PAGE_ID", "11316539111111111111");
 define("FB_ACCESS_TOKEN", "EAAFZCUnqiOn0BAHPGLStkMMWCPH2kOeZBZAjxEKNvIx8bZAF8u5P5ftrexUJPE2U0s8g1d45svbaXKz3DZBzX2uGJDCNRgtotpWB3VI5Qm6lZCbGurTTm0xtGHxt5MJp2KJZCK3dQZDZD");
-
+define("PID_PATH",dirname(__DIR__)."/mypid.pid");
 
 $config['db_host'] = 'localhost';
 $config['db_username'] = 'ts-emt';
