f6771b66203d8520899e235ca4ff162f62efae62
minhsieh <side112358@gmail.com>
2019-03-15 17:15:38 +0800

    finish GetAlarm: get alarms & publish to facebook

--- app/Console/Commands/GetAlarm.php
+++ app/Console/Commands/GetAlarm.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Modules\Cockpit\Services\ParserDom;
+use App\Alarm;
 
 class GetAlarm extends Command
 {
@@ -52,7 +53,7 @@ class GetAlarm extends Command
         foreach($tds as $key => $one){
             $two = $one->find("td");
             $result[] = [
-                'time' => str_replace ("&nbsp;&nbsp;&nbsp;&nbsp;"," ",$two[0]->getPlainText()),
+                'time' => str_replace ("&nbsp;&nbsp;&nbsp;"," ", $two[0]->getPlainText()),
                 'type' => $two[1]->getPlainText(),
                 'team' => $two[2]->getPlainText(),
                 'status' => $two[3]->getPlainText(),
@@ -62,7 +63,36 @@ class GetAlarm extends Command
             ];
         }
 
-        //
-        print_r($result);
+        $valuestore = app('valuestore');
+        $page_token = $valuestore->get('page_access_token');
+
+        foreach($result as $one){
+            // Save to DB
+            $alarm = Alarm::firstOrNew(['time' => $one['time'] , 'location' => $one['location']]);
+
+            $alarm->type = $one['type'];
+            $alarm->team = $one['team'];
+            $alarm->status = $one['status'];
+            $alarm->location = $one['location'];
+            $alarm->save();
+        }
+
+        $pend_alarm = Alarm::whereNull('publish_at')->orderBy('created_at','DESC')->take(10)->get();
+
+        echo "Pend Publish:".count($pend_alarm).PHP_EOL;
+
+        foreach($pend_alarm as $alarm){
+            $message = $alarm->type."\n".$alarm->time."\n出動分隊：".$alarm->team."\n案件地點：".$alarm->location."\n\n\n#即時消防\n#".$alarm->team;
+            $types = explode("-",$alarm->type);
+            if(!empty($types[0])){
+                $message = $message."\n#".$types[0];
+            }
+            if(!empty($types[1])){
+                $message = $message."\n#".$types[1];
+            }
+            $response = $client->request('POST','https://graph.facebook.com/113165399264137/feed?message='.urlencode($message).'&access_token='.$page_token);
+            $alarm->publish_at = date("Y-m-d H:i:s");
+            $alarm->save();
+        }
     }
 }
